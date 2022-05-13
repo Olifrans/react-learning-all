dd291bc18eddc7d9f467f7eb8f18e168db9760b3
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-13 09:39:31 -0300

    feat: Componente simplificado

--- src/components/ComponenteTres.tsx
+++ src/components/ComponenteTres.tsx
@@ -1,9 +1,7 @@
 
 
-export default () => {
-  return (
-    <div className="componente">
-      <p className="texto">Exibição do Componente Três</p>
-    </div>
-  );
-};
+export default props =>
+  <div className="componente">
+    <p className="texto">Exibição do Componente Três</p>
+  </div>
+
