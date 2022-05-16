da95a86d3223fdabcc916c4a9a7c68bf60d257c4
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-16 11:14:11 -0300

    feat:  Spread Operator:

--- src/comunicacao_direta-dois/ComponenteFilhoA.tsx
+++ src/comunicacao_direta-dois/ComponenteFilhoA.tsx
@@ -1,7 +1,7 @@
 export default function ComponenteFilhoA(props) {
   return (
     <div className="componente">
-      <p>Filho: {props.nome}</p>
+      <p>Filho: {...props}</p>
     </div>
   );
 }
