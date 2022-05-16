6e7c41ec2f88dead712693b8bdcc429dbaf31253
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-16 11:43:51 -0300

    feat: Teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.css";
 
 function App() {
+  var valor = 0;
+
+  function incrementar() {
+    valor++;
+    console.log(valor);
+  }
   return (
     <>
-    <h1>Reacct Hooks</h1>
-
-
-
+      <h1>Reacct Hooks</h1>
+      <h3>{valor}</h3>
+      <button onClick={incrementar}>Incrementar</button>
     </>
   );
 }
