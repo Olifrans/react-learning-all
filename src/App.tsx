a2e70f4b9434a2e4d90375319c4ad59fc55bc152
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-16 12:34:37 -0300

    feat: forma de usar o useState

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,39 @@ function App() {
   const [valor, setValor] = useState(77);
 
   function incremento() {
-    setValor(valor + 1);
+    setValor((valor_anterior) => valor_anterior + 1);
+    setValor((valor_anterior) => valor_anterior + 1);
+    setValor((valor_anterior) => valor_anterior + 1);
   }
 
   function decremento() {
-    setValor(valor - 1);
+    setValor((valor_anterior) => valor_anterior - 1);
+    setValor((valor_anterior) => valor_anterior - 1);
+    setValor((valor_anterior) => valor_anterior - 1);
   }
 
   return (
     <>
-      <h1>Reacct Hooks</h1>
-      <h3>Valor: {valor}</h3>
+      <h1>Reacct Hooks - useState</h1>
       <button onClick={incremento}>Incremento</button>
-      <br></br>
+      <span>{valor}</span>
       <button onClick={decremento}>Decremento</button>
+
+      <button
+        onClick={() => {
+          setValor(valor + 10);
+        }}
+      >
+        Incremento
+      </button>
+
+      <button
+        onClick={() => {
+          setValor((valor_anterior) => valor_anterior + 777);
+        }}
+      >
+        Incremento
+      </button>
     </>
   );
 }
